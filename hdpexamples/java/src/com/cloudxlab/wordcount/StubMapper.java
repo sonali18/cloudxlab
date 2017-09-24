48d82dddc6634aabc06a806f99f32e982d3cf02f
sonali18 <sonali.18.dubey@gmail.com>
2017-09-24 09:06:21 +0530

    Update StubMapper.java

--- hdpexamples/java/src/com/cloudxlab/wordcount/StubMapper.java
+++ hdpexamples/java/src/com/cloudxlab/wordcount/StubMapper.java
@@ -10,9 +10,8 @@ import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapred.*;
 
 public class StubMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-
+private final static IntWritable one = new IntWritable(1);
   @Override
-
 	public void map(LongWritable key, Text value, OutputCollector <Text, IntWritable> output, Reporter reporter) throws IOException {
 
 		String valueString = value.toString();
